1906bf9aa77720da8741490f4a183ce82dcc1c25
unknown <mick.altura@gmail.com>
2021-05-03 04:10:38 +0200

    Add Graph Api->Two Methods

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask, render_template
+from random import gauss
+from flask import Flask, render_template, send_file
 from string import ascii_letters
 from utils.cr import Client
+from utils.gr import graphing, graphing_random
 app = Flask(__name__)
+
+app.jinja_env.globals.update(gauss=gauss)
 
 
 @app.route('/rsa/<passw>')
 def index(passw):
-    return render_template('base.html', client=Client(passw) )
+    return render_template('base.html', client=Client(passw))
+
+
+@app.route('/graph/<a>/<b>/<c>/<n>/<l>')
+def enc(a, b, c, n, l):
+    try:
+        graphing(int(a), int(b), int(c), int(n), int(l))
+        return send_file('tmp\graph.png')
+    except:
+        return render_template('index.html')
+
+
+@app.route('/graph/r')
+def graphing_r():
+    try:
+        graphing_random()
+        return send_file('tmp\graph.png')
+    except:
+        return render_template('index.html')
 
 
 app.run(debug=True)
---
+++ utils/gr.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from random import choice
+from matplotlib import rcParams
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = ['Verdana']
+
+def graphing(a=0,b=0,c=0,n=1,l=5):
+    x=np.linspace(-l,l,1000)
+    y = a*x**n+b*x+c
+    fig, ax = plt.subplots()
+    ax.plot(x, y, label=r"$ x^2 $")
+    ax.spines['left'].set_position('zero')
+    ax.spines['bottom'].set_position('zero')
+    plt.savefig('tmp\graph.png')
+def graphing_random():
+    x=np.linspace(-5,5,100)
+    fig, ax = plt.subplots()
+    ax.spines['bottom'].set_position('zero')
+    ax.spines['left'].set_position('zero')
+    ax.grid(True)
+    ax.set_ylim(-5,5)
+    ax.set_xlim(-5,5)
+    ax.set_title(r"$ax^e+bx+c$")
+    for i in range(-3,3):
+        n = [-3,-2,2,3]
+        a = choice(n)
+        b= choice(n)
+        c = choice(n)
+        e = choice(n)
+        y = a*x**e+b*x+c
+        if b>0:
+            b = "+"+str(b)
+        if c>0:
+            c = "+"+str(c)
+
+        ax.plot(x,y, label=f"${a}x^{ {e} }{b}x{c}$")
+    ax.legend()
+    plt.savefig('tmp\graph.png')
+    plt.close()
